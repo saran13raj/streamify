41b11588d034d0fc6584fa82a5f31321cef332bf
saran13raj <saran13raj@gmail.com>
2024-09-07 21:51:25 +0530

    add unit tests

---
+++ src/shared/components/__mocks__/shared/components/shadcn/ui/chart.tsx
@@ -0,0 +1,4 @@
+export const ChartConfig = jest.fn(() => ({}));
+export const ChartContainer = jest.fn(({ children }) => <div>{children}</div>);
+export const ChartTooltip = jest.fn(({ children }) => <div>{children}</div>);
+export const ChartTooltipContent = jest.fn(() => <div />);
